3dca901019b510aee3ec90ec3a6cfe408ff1dcca
GambuzX <mg_mesquita@hotmail.com>
2020-06-11 16:25:20 +0100

    Gun, shooting and muzzle effect

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,8 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
-    [SerializeField] float chaseRange = 10f;
+    [SerializeField] float chaseRange = 5f;
+    bool isProvoked = false;
 
     private NavMeshAgent navMeshAgent;
     private float distanceToTarget = Mathf.Infinity;
@@ -19,8 +20,30 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distanceToTarget = Vector3.Distance(transform.position, target.position);
-        if (distanceToTarget <= chaseRange)
-            navMeshAgent.SetDestination(target.position);
+        if (isProvoked) {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange) {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget() {
+
+        if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+            ChaseTarget();
+        }
+        else {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget() {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget() {
+        Debug.Log("Attacking player");
     }
 
     void OnDrawGizmosSelected() {
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 20f;
+    [SerializeField] ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        RaycastHit hit;
+        if(!Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) return;
+
+        EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+        if(target) {
+            PlayMuzzleFlash();
+            target.Hit(damage);
+        }
+    }
+
+    private void PlayMuzzleFlash() {
+        muzzleFlash.Play();
+    }
+}
