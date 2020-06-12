29a86c51e7ee9b18c011fee5d9795bb10554127e
GambuzX <mg_mesquita@hotmail.com>
2020-06-12 19:44:59 +0100

    weapon zoom

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomedInMouseSensitivity = .5f;
+    [SerializeField] float zoomedOutMouseSensitivity = 2f;
+
+    bool zoomedInToggle = false;
+    FirstPersonController firstPersonController;
+
+    void Start() {
+        firstPersonController = GetComponent<FirstPersonController>();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            fpsCamera.fieldOfView = zoomedInToggle ? zoomedOutFOV: zoomedInFOV;
+            firstPersonController.m_MouseLook.XSensitivity = zoomedInToggle ? zoomedOutMouseSensitivity : zoomedInMouseSensitivity;
+            firstPersonController.m_MouseLook.YSensitivity = zoomedInToggle ? zoomedOutMouseSensitivity : zoomedInMouseSensitivity;
+            zoomedInToggle = !zoomedInToggle;
+        }
+    }
+}
