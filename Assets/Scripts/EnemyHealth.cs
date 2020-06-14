2303a57b864fb9517de1f9273e77a07318093adb
GambuzX <mg_mesquita@hotmail.com>
2020-06-14 18:44:57 +0100

    Enemy now a zombie

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,11 +6,22 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float health = 100f;
 
+    bool hasDied = false;
+
     public void Hit(float damage) {
+        if(hasDied) return;
         BroadcastMessage("OnDamageTaken");
         health -= damage;
         if (health <= 0) {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die() {
+        if(hasDied) return;
+        hasDied = true;
+        GetComponent<Animator>().SetTrigger("die");
+        GetComponent<EnemyAI>().enabled = false;
+        GetComponent<CapsuleCollider>().enabled = false;
     }
 }
